7a7b9831ac2d9bdf9f539decbb5aa1f1667bd0ec
Mousam <mousam.kalita777@gmail.com>
2022-05-01 17:19:02 +0530

    changed fetchEpisodes function

--- src/screens/CharacterDetails/CharacterDetails.js
+++ src/screens/CharacterDetails/CharacterDetails.js
@@ -30,7 +30,8 @@ function CharacterDetails() {
 		};
 		fetchData();
 
-		// fetching origin data for a particular character    
+		// fetching origin data for a particular character
+
 		const fetchOriginData = async () => {
 			try {
 				let response = await axios.get(originApi);
@@ -41,25 +42,67 @@ function CharacterDetails() {
 			}
 		};
 
-		if(originApi)
-		{
+		if (originApi) {
 			fetchOriginData();
 		}
-        
+
 		// calling every api in the "episode" array and adding the chapter title to the chapter array using IIFE
-		if (data.episode) {
-			data.episode.forEach(item => {
-				(async () => {
-					try {
-						let response = await axios.get(item);
-						setChapters(prev => [...prev, response.data.name]);
-					}
-					catch (error) {
-						console.log(error);
-					}
-				})();
+		// if (data.episode) {
+		// 	data.episode.forEach(item => {
+		// 		(async () => {
+		// 			try {
+		// 				console.log(item);
+		// 				let lastChar = item.substring(item.lastIndexOf("/") + 1);
+		// 				console.log(lastChar);
+		// 				let response = await axios.get(item);
+		// 				setChapters(prev => [...prev, response.data.name]);
+		// 			}
+		// 			catch (error) {
+		// 				console.log(error);
+		// 			}
+		// 		})();
+		// 	});
+		// }
+
+		// fetching episodes
+		let fetchEpisodes = async () => {
+			let episodes = [];
+			data?.episode?.forEach(item => {
+				let lastChar = item.substring(item.lastIndexOf("/") + 1);
+				episodes.push(lastChar);
+
 			});
-		}	
+
+			let episodeList = episodes.join();
+			
+			try {
+				let res = await axios.get(`https://rickandmortyapi.com/api/episode/${episodeList}`);
+
+				if (Array.isArray(res.data)) {
+					let data = Array.from(res.data);
+					let chapters = data.map(item => {
+						return item.name;
+					});
+
+					setChapters(prev => [...prev, ...chapters]);
+				}
+				else{
+
+					setChapters(prev => [...prev, res.data.name]);
+				}
+
+			}
+			catch (error) {
+				console.log(error);
+			}
+		};
+		if (data.episode) {
+			fetchEpisodes();
+		}
+
+
+
+
 	}, [api, originApi]);
 
 	return (
@@ -78,7 +121,7 @@ function CharacterDetails() {
 					<h2>Chapters</h2>
 					<ul>
 						{/* Iterating through each item in the chapter array to print every episode name for that particular character */}
-						{chapter.map((item,index) => {
+						{chapter.map((item, index) => {
 							return <li key={index}>{item}</li>;
 						})}
 					</ul>
